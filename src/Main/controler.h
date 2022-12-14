ba5b0b482c813b66458a581d62611d9c119a8250
DinossauroBebado <guipsilva32@gmail.com>
2022-12-14 17:04:50 -0300

    add controler

--- src/Main/controler.h
+++ src/Main/controler.h
@@ -4,15 +4,41 @@
 class controler
 {
 private:
-    /* data */
+    float proporcional();
+    float integrative();
+    float derivative();
 public:
-    controler(/* args */);
-   
+    float KP;
+    float KD;
+    float KI;
+    float error;
+    float time; 
+    float output_value;
+
+    controler(float kp, float kd, float ki);
+    float output(float input_value, float current_value);
+
+
 };
 
-controler::controler(/* args */)
-{
+controler::controler(float kp, float kd, float ki)
+{   KP = kp;
+    KD = kd;
+    KI = ki;
 }
+
+float controler::output(float input_value, float current_value){
+    error = current_value - input_value;
+    time = micros();
+    output_value = proporcional();
+
+}
+
+float controler::proporcional(){
+    return error*KP; 
+
+}
+
 
 
 
